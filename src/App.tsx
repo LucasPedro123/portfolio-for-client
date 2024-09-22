bcae232fcb1a3df9bfe1b1f9e9c9b5fe720cedb2
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-09-21 22:51:09 -0300

    feat: created ancestral archive

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import AppStyle from "./assets/styles"
+import Router from "./routes"
+
+function App() {
+
+  return (
+    <>
+      <AppStyle />
+      <Router />
+    </>
+  )
+}
+
+export default App
