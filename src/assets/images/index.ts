e747b27aaa04052dbdc46f8b0041b7e54a795360
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-09-21 23:00:34 -0300

    chore: created image export file

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export {default as logoDefault} from './logo-default.svg'
+export {default as logoOutline} from './logo-outline.svg'
+export {default as imageMain} from './image-hero.png'
+export {default as dotsImage} from './dots-table.png'
