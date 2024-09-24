776a6177c8a27a07f4f6ca95cee40a667369be05
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-09-24 20:32:55 -0300

    style(global): added font-family and centering of elements on screen

--- src/assets/styles/index.tsx
+++ src/assets/styles/index.tsx
@@ -3,10 +3,15 @@ import { global } from './global.style'
 
 const AppStyle = createGlobalStyle`
     body{
+        font-family: 'Fira Code';
         background-color: ${global.color.background};
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        overflow-x: hidden;
     }
 `
 
